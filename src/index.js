eb9250bc0612781e09945be3d2fc2a1eb5fe377b
masete <nicholasmasete72@gmail.com>
2022-07-16 19:46:00 +0300

    i learnt the hardway the diff btn tab and space in eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import FirebaseContext from './context/firebase';
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>
+);
