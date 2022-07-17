247a1aa735ede06220b2b90509533516e409d6e5
masete <nicholasmasete72@gmail.com>
2022-07-17 16:07:10 +0300

    recfactored to new root api forat

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import { useMemo, React } from 'react';
 // import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { firebase, FieldValue } from './lib/firebase';
 import FirebaseContext from './context/firebase';
 
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+<FirebaseContext.Provider value={{ firebase, FieldValue }}>
+  <App />
+</FirebaseContext.Provider>
+);
 // ReactDOM.render(
 //   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
 //     <App />
@@ -12,12 +21,14 @@ import FirebaseContext from './context/firebase';
 //   document.getElementById('root')
 // );
 
-ReactDOM.render(<FireBaseWrapper />, document.getElementById('root'));
+// ReactDOM.render(<FireBaseWrapper />, document.getElementById('root'));
 
-const FireBaseWrapper = () => {
-  const fireBaseProviderValue= useMemo(() => ({ firebase, FieldValue }), [firebase, FieldValue]);
+// const FireBaseWrapper = () => {
+//   const fireBaseProviderValue= useMemo(() => ({ firebase, FieldValue }), [firebase, FieldValue]);
 
-return (<FirebaseContext.Provider value={fireBaseProviderValue}>
-    <App />
-  </FirebaseContext.Provider>)
-}
+// return (<FirebaseContext.Provider value={fireBaseProviderValue}>
+//     <App />
+//   </FirebaseContext.Provider>)
+// }
+
+
