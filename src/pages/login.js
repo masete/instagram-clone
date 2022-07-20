e4750c8aeeda5d40e9f0eca7c423d1549ed9c03e
masete <nicholasmasete72@gmail.com>
2022-07-20 23:33:15 +0300

    added landing page iage

--- src/pages/login.js
+++ src/pages/login.js
@@ -23,7 +23,19 @@ export default function Login() {
     }
 
     return (
-        <p>This is the login page</p>
+    
+        <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+            <div className='flex w-3/5'>
+                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+            </div>
+            <div className='flex flex-col w-2/5'>
+                <p>I will be the form</p>
+            </div>
+            <div className='flex flex flex-col w-1/5'>
+                <p>cheiiiiii</p>
+            </div>
+        </div>
+    
     )
 };
 
