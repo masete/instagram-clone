b670640bbf06599367f35a50655752c0c4e21a86
masete <nicholasmasete72@gmail.com>
2022-07-20 18:25:20 +0300

    added firebase context in login logic

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,29 @@
-const Login = () => {
+import { getByTitle } from '@testing-library/react';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+
+    const navigate = useNavigate();
+    const { firebase } = useContext(FirebaseContext)
+
+    const [emailAddress, setEmailAddress] = useState();
+    const [password, setPassword] = useState();
+
+    const[error, setError] = useState();
+    const IsInvalid = password === "" || emailAddress === "";
+
+    useEffect(() => {
+        document.title = 'Login - Instagram'
+    }, [])
+
+    const handleLogin = () =>{
+
+    }
+
     return (
         <p>This is the login page</p>
     )
 };
 
-export default Login;
