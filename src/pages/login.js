01aaf002b013543b16c3b0b6fd9a5354be19795d
masete <nicholasmasete72@gmail.com>
2022-07-22 00:03:09 +0300

    added password input field

--- src/pages/login.js
+++ src/pages/login.js
@@ -38,7 +38,19 @@ export default function Login() {
             aria-label="Please enter your email address"
             type='text'
             placeholder='Email address'
-            className='text-sm text-gray-primary'/>
+            className='text-sm text-gray-primary w-full mr-3 py-5 px-4 h-2 border 
+            border-gray-primary rounded mb-2'
+            onChange={({ target })=> setEmailAddress(target.value)}
+            />
+
+            <input
+            aria-label="Please enter your password"
+            type='password'
+            placeholder='Password'
+            className='text-sm text-gray-primary w-full mr-3 py-5 px-4 h-2 border 
+            border-gray-primary rounded mb-2'
+            onChange={({ target })=> setPassword(target.value)}
+            />
           </form>
           </div>
          
