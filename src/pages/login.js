efb3f5c07e2aa444fe808713ec2fc8f3a960f9af
masete <nicholasmasete72@gmail.com>
2022-07-23 19:51:03 +0300

    working on the tailwind config

--- src/pages/login.js
+++ src/pages/login.js
@@ -57,7 +57,7 @@ export default function Login() {
             <button
               disabled={isInvalid}
               type="submit"
-              className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
             ${isInvalid && 'opacity-50'}`}
             >
               Login
