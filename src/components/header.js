11b78118fa9286a9d4cc719dc3d67cb815d87966
masete <nicholasmasete72@gmail.com>
2022-07-27 11:53:06 +0300

    added instagra logo on the header

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,16 @@ const Header = () =>{
         <>
         <header className="h-16 bg-white border-b border-gray-primary mb-8">
             <div className="container mx-auto mx-w-screen-lg h-full">
-                <div className="flex justify-between h-full"></div>
+                <div className="flex justify-between h-full">
+                    <div className="text-gray-700 text-center flex items-center align-item cursor-pointer">
+                        <h1 className="flex justify-center w-full">
+                            <Link to={ROUTES.DASHBOARD} aril-label = 'Instagram'>
+                            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12"/>
+                            </Link>
+                        </h1>
+                    </div>
+                    <div className="text-gray-700 text-center flex items-center align-items">hey</div>
+                </div>
             </div>
         </header>
         </>
