5adac58313afde8da37867b755465cd1d11c950d
masete <nicholasmasete72@gmail.com>
2022-07-27 11:34:02 +0300

    working on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+import UserContext from "../context/user";
+import * as ROUTES from '../constants/routes'
+
 const Header = () =>{
+    const { firebase } = useContext(FirebaseContext)
+    // const { user } = useContext(UserContext)
+
     return (
         <>
-        <div>
-            <p>i a the header</p>
-        </div>
+        <header className="h-16 bg-white border-b border-gray-primary mb-8">
+            <div className="container mx-auto mx-w-screen-lg h-full">
+                <div className="flex justify-between h-full"></div>
+            </div>
+        </header>
         </>
     )
 };
