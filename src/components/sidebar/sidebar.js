e93a9935a8027b9129cef79fdaebd5ef46dc751a
masete <nicholasmasete72@gmail.com>
2022-07-28 11:35:14 +0300

    chore: re-organised the sidebar into a folder

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,9 @@
+import useUser from '../../hooks/use-user'
+
+
 const Sidebar = () => {
+    const { user: {fullName, username, userId} } = useUser();
+    console.log('user',user)
     return (
         <div>
             <p>I a the side bar</p>
