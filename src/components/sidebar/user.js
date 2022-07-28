ca7d67a32363403516d254d51a438882ccfbc4b7
masete <nicholasmasete72@gmail.com>
2022-07-28 19:40:31 +0300

    fix: not fetching user data fro firebase

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -8,7 +8,6 @@ const User = ({username, fullName}) => {
         <Skeleton count={1} height={64} />
     ): (
         <Link to={`/p/${username}`} className='grid grid-cols-4 gap-4 mb-6 items-center'>
-            console.log("username", username);
             <div className='flex items-center justify-between col-span-1'></div>
             <img className='rounded-full w-16 flex mr-3' 
             src={`/images/avatars/${username}.jpg`}/>
